5b7ce2937b92450fc8ff35f5e72bf631b006680f
DESKTOP-DFIROVA\tjdgn <hvh6634@naver.com>
2023-07-24 16:38:40 +0900

    조형훈 커밋

--- Project M/Assets/Script/Resource/ResourceManager.cs
+++ Project M/Assets/Script/Resource/ResourceManager.cs
@@ -1,18 +1,46 @@
+using ProjectM.Util;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.U2D;
 using UnityEngine;
+using UnityEngine.U2D;
 
-public class ResourceManager : MonoBehaviour
+/// <summary>
+/// 런타임에 필요한 리소스를 불러오는 기능을 가진 클래스
+/// </summary>
+public class ResourceManager : Singleton<ResourceManager>
 {
-    // Start is called before the first frame update
-    void Start()
+    public void Initialize()
     {
-        
+        // LoadAllAtlas();
+        // LoadAllPrefabs();
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// 경로를 이용해서 리소스 폴더 안의 프리팹을
+    /// 가져오는 메서드
+    /// </summary>
+    /// <param name="path"> 사용할 경로 </param>
+    /// <returns></returns>
+    public GameObject LoadObject(string path)
     {
-        
+        // 에셋 폴더 안에 리소스 폴더가 존재한다면
+        // 해당 경로로 부터 path를 읽어옵니다.
+        // 해당 경로에 파일이 GameObject 형태로 부를 수 있다면
+        // 불러온다.
+        return Resources.Load<GameObject>(path);
+    }
+
+    /// <summary>
+    /// 리소스 폴더 안의 모든 아틀라스를 불러와
+    /// 스프라이트 로더에 등록하는 메서드
+    /// </summary>
+    private void LoadAllAtlas()
+    {
+        var portraitAtlas = Resources.LoadAll<SpriteAtlas>("Atlase/PortraitAtlase");
+        // SpriteLoader.SetAtlas(portraitAtlas);
+
+        var uiAtlas = Resources.LoadAll<SpriteAtlas>("Atlase/UIAtlase");
+        // SpriteLoader.SetAtlas(uiAtlas);
     }
 }
